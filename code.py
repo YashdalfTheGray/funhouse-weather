b992252a8b8c56fb6976717f5c612c00b39e5221
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-08-07 19:09:33 -0400

    added temperature readout to the screen

--- code.py
+++ code.py
@@ -1,72 +1,38 @@
-# commit3
-# changed a lot about how the text is rendered on screen
-
-import ssl
-import wifi
-import socketpool
-import adafruit_requests
+import board
 import time
+import adafruit_dps310
+import adafruit_ahtx0
 
 from secrets import secrets
 from adafruit_funhouse import FunHouse
 
-# some functions to help set everything up
+# some global labbels/state that will exist
+value_label = None
+unit_label = None
 
-
-def get_current_time(requests):
-    TIME_URL = f"https://io.adafruit.com/api/v2/{aio_username}/integrations/time/strftime?x-aio-key={aio_key}"
-    TIME_URL += "&fmt=%25A|%25b%20%25e%2C%20%25Y|%25I%3A%25M%20%25p%20%25Z"
-
-    response = requests.get(TIME_URL)
-    return response.text.split("|")
-
-
-def get_default_text_style():
-    return {
-        "text_color": 0xFFFFFF,
-        "text_anchor_point": (0, 0),
-        "text_scale": 3,
-    }
-
+# set up the sensors
+i2c = board.I2C()
+dps310 = adafruit_dps310.DPS310(i2c)
+aht20 = adafruit_ahtx0.AHTx0(i2c)
 
 # setup the funhouse controller
 funhouse = FunHouse(default_bg=0x000000)
 
-# clear the display
+# clear the display and lights, and then show the splash screen
 funhouse.display.show(None)
-
-# get our username, key and desired timezone
-aio_username = secrets["aio_username"]
-aio_key = secrets["aio_key"]
-location = secrets.get("timezone", None)
-
-# enable and connect to the wifis
-wifi.radio.stop_scanning_networks()
-wifi.radio.connect(secrets["ssid"], secrets["password"])
-# print("Connected to %s!" % secrets["ssid"])
-# print("My IP address is", wifi.radio.ipv4_address)
-
-# create a request pool and something to send requests with
-pool = socketpool.SocketPool(wifi.radio)
-requests = adafruit_requests.Session(pool, ssl.create_default_context())
-
-current_day, current_date, current_time = get_current_time(requests)
-
-day_label = funhouse.add_text(
-    text=current_day, text_position=(5, 0), **get_default_text_style()
-)
-date_label = funhouse.add_text(
-    text=current_date, text_position=(5, 50), **get_default_text_style()
-)
-time_label = funhouse.add_text(
-    text=current_time, text_position=(5, 100), **get_default_text_style()
-)
+funhouse.peripherals.set_dotstars(
+    0x000000, 0x000000, 0x000000, 0x000000, 0x000000)
 funhouse.display.show(funhouse.splash)
 
+# setup some labels for the screen
+value_label = funhouse.add_text(
+    text=f"{dps310.temperature:.1f}",
+    text_position=(120, 120),
+    text_scale=6,
+    text_color=0xFFFFFF,
+    text_anchor_point=(0.5, 0.5)
+)
 
 while True:
-    time.sleep(5)
-    current_day, current_date, current_time = get_current_time(requests)
-    funhouse.set_text(current_day, day_label)
-    funhouse.set_text(current_date, date_label)
-    funhouse.set_text(current_time, time_label)
+    funhouse.set_text(f"{dps310.temperature:.1f}", value_label)
+    time.sleep(0.5)
