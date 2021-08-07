34780784648c1e91e60dce3e468a9c6ca1989fbf
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-08-07 13:05:48 -0400

    clean up and fetch just the time

--- code.py
+++ code.py
@@ -1,21 +1,12 @@
-import ipaddress
+# commit1
+# clean up and fetch just the time
+
 import ssl
 import wifi
 import socketpool
 import adafruit_requests
-import secrets
 
-
-TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
-JSON_QUOTES_URL = "https://www.adafruit.com/api/quotes.php"
-JSON_STARS_URL = "https://api.github.com/repos/adafruit/circuitpython"
-
-# Get wifi details and more from a secrets.py file
-try:
-    from secrets import secrets
-except ImportError:
-    print("WiFi secrets are kept in secrets.py, please add them there!")
-    raise
+from secrets import secrets
 
 # Get our username, key and desired timezone
 aio_username = secrets["aio_username"]
@@ -25,23 +16,10 @@ TIME_URL = "https://io.adafruit.com/api/v2/%s/integrations/time/strftime?x-aio-k
     aio_username, aio_key)
 TIME_URL += "&fmt=%25Y-%25m-%25d+%25H%3A%25M%3A%25S.%25L+%25j+%25u+%25z+%25Z"
 
-print("ESP32-S2 Adafruit IO Time test")
-
-print("My MAC addr:", [hex(i) for i in wifi.radio.mac_address])
-
-print("Available WiFi networks:")
-for network in wifi.radio.start_scanning_networks():
-    print("\t%s\t\tRSSI: %d\tChannel: %d" % (str(network.ssid, "utf-8"),
-                                             network.rssi, network.channel))
 wifi.radio.stop_scanning_networks()
-
-print("Connecting to %s" % secrets["ssid"])
 wifi.radio.connect(secrets["ssid"], secrets["password"])
-print("Connected to %s!" % secrets["ssid"])
-print("My IP address is", wifi.radio.ipv4_address)
-
-ipv4 = ipaddress.ip_address("8.8.4.4")
-print("Ping google.com: %f ms" % wifi.radio.ping(ipv4))
+# print("Connected to %s!" % secrets["ssid"])
+# print("My IP address is", wifi.radio.ipv4_address)
 
 pool = socketpool.SocketPool(wifi.radio)
 requests = adafruit_requests.Session(pool, ssl.create_default_context())
